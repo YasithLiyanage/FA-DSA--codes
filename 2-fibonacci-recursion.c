9fcdaaa535f8bce680538745278085f7419db88f
YasithLiyanage <hlyanjana2321@gmail.com>
2025-10-21 02:04:31 +0530

    updated

--- 2-fibonacci-recursion.c
+++ 2-fibonacci-recursion.c
@@ -3,8 +3,8 @@
 int fibonacci(int n)
 {
 
-    if (n==0){
-        return 0;
+    if (n<=1){
+        return n;
     }else{
 
         return fibonacci(n-1) + fibonacci(n-2);
--- lab1.1.c
+++ lab1.1.c
@@ -1,24 +1,50 @@
 #include <stdio.h>
 
-void insertion_sort(int arr[], int n){
-    int i, j,key;
-    for(j=1;j>n;j++){
-        
-    
+
+void insertion_sort(int arr[],int n){
+
+    int i,j,key;
+    for (j=1; j<n; j++){
+
+        key = j;
+        i = j - 1;
+
+
+            while( i >= 0 && arr[i] > key){
+                arr[i + 1] = arr[i];
+                i--; 
+
+            } 
+
+        arr[i+1] = key;
 
     }
-
-
-
 
 }
 
 
+void printArr(int arr[]){
+
+for(int l=0; l<7; l++){
+    printf("%d\n",arr[l]);
+
+}
+
+}
+
+
+
 int main(){
 
+int arr[7] = {8,2,4,7,3,5,1}; 
 
+printf("Unsorted is\n");
+printArr(arr);
 
+insertion_sort(arr, 7);
 
+printf("Sorted is\n");
+printArr(arr);
 
 
 }
